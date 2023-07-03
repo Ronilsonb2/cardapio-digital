656799e9a0c849e758ad94866070cbbbe8656a2d
Ronilsonb2 <ronilson-b2@hotmail.com>
2023-07-03 20:37:53 -0300

    alteracões aplicadas na estrututura, e layout modificado

--- js/dados_produtos.js
+++ js/dados_produtos.js
@@ -1,7 +1,8 @@
 const dados_cardapio = [
     {
-      "categoria": "ofertas_do_dia",
-      "pratos": [
+      "categoria": "ofertas-do-dia",
+      "title": "Ofertas do dia",
+      "itens": [
         {
             nome: "Lagosta ao thermidor",
             descricao: "Lagosta feita com creme de leite, cenoura, cebola, conhaque e queijo",
@@ -53,189 +54,263 @@ const dados_cardapio = [
       ]
     },
     {
-      "categoria": "semanais",
-      "pratos": [
-        {
-            nome: "Lagosta ao thermidor",
-            descricao: "Lagosta feita com creme de leite, cenoura, cebola, conhaque e queijo",
-            imagem: "assets/prato1.jpg",
-            preco: "19,90"
-        },
-        {
-            nome: "Feijão tropeiro",
-            descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
-            imagem: "assets/prato2.jpg",
-            preco: "19,90"
-        },
-        {
-            nome: "Espaguete ao frutos do mar",
-            descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
-            imagem: "assets/prato3.jpg",
-            preco: "24,90"
-        },
-        {
-            nome: "Camarão na champagne com arroz de maçã",
-            descricao: "Camarão ao molho, cebola, creme de leite, caldo de peixe e champagne.",
-            imagem: "assets/prato1.jpg",
-            preco: "29,90"
-        },
-        {
-            nome: "Paella",
-            descricao: "Saborosa e tradicional paella que leva camarão, mexilhões, lula e polvo.",
-            imagem: "assets/prato2.jpg",
-            preco: "9,90"
-        },
-        {
-            nome: "Lula grelhada com arroz negro",
-            descricao: "Acompanhado com o delicioso arroz negro ao vinho branco e parmesão",
-            imagem: "assets/prato3.jpg",
-            preco: "49,90"
-        },
-        {
-            nome: "Carne de panela",
-            descricao: "Lagosta feita com creme de leite, cenoura, cebola, conhaque e queijo acompanhado de arroz branco.",
-            imagem: "assets/prato1.jpg",
-            preco: "69,90"
-        }
-      ]
+        "categoria": "semanais",
+        "title": "Itens diários",
+        "itens": [
+            {
+                nome: "Lagosta ao thermidor",
+                descricao: "Lagosta feita com creme de leite, cenoura, cebola, conhaque e queijo",
+                imagem: "assets/prato1.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Camarão na champagne com arroz de maçã",
+                descricao: "Camarão ao molho, cebola, creme de leite, caldo de peixe e champagne.",
+                imagem: "assets/prato1.jpg",
+                preco: "29,90"
+            },
+            {
+                nome: "Paella",
+                descricao: "Saborosa e tradicional paella que leva camarão, mexilhões, lula e polvo.",
+                imagem: "assets/prato2.jpg",
+                preco: "9,90"
+            },
+            {
+                nome: "Lula grelhada com arroz negro",
+                descricao: "Acompanhado com o delicioso arroz negro ao vinho branco e parmesão",
+                imagem: "assets/prato3.jpg",
+                preco: "49,90"
+            },
+            {
+                nome: "Carne de panela",
+                descricao: "Lagosta feita com creme de leite, cenoura, cebola, conhaque e queijo acompanhado de arroz branco.",
+                imagem: "assets/prato1.jpg",
+                preco: "69,90"
+            }
+        ]
     },
     {
-      "categoria": "sobremesas",
-      "pratos": [
-        {
-            nome: "Frozen Yogurt",
-            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-            imagem: "assets/prato1.jpg",
-            preco: "19,90"
-        },
-        {
-            nome: "Suflê de Banana e Canela",
-            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-            imagem: "assets/prato2.jpg",
-            preco: "19,90"
-        },
-        {
-            nome: "Mousse de Chocolate Light",
-            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-            imagem: "assets/prato3.jpg",
-            preco: "24,90"
-        },
-        {
-            nome: "Petit Gâteau Tradicional",
-            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-            imagem: "assets/prato1.jpg",
-            preco: "29,90"
-        },
-        {
-            nome: "Crème Brûlée",
-            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-            imagem: "assets/prato2.jpg",
-            preco: "9,90"
-        }
-      ]
+        "categoria": "sobremesas",
+        "title": "Sobremesas",
+        "itens": [
+            {
+                nome: "Lagosta ao thermidor",
+                descricao: "Lagosta feita com creme de leite, cenoura, cebola, conhaque e queijo",
+                imagem: "assets/prato1.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            }
+          ]
+    },
+    {
+        "categoria": "bebidas",
+        "title": "Bebidas",
+        "itens": [
+            {
+                nome: "Lagosta ao thermidor",
+                descricao: "Lagosta feita com creme de leite, cenoura, cebola, conhaque e queijo",
+                imagem: "assets/prato1.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            },
+            {
+                nome: "Feijão tropeiro",
+                descricao: "Deliciosa, acompanha arroz, feijão, couve refogada e fritas",
+                imagem: "assets/prato2.jpg",
+                preco: "19,90"
+            },
+            {
+                nome: "Espaguete ao frutos do mar",
+                descricao: "Espaguete com camarão, mexilhões, lula e polvo ao molho do chef.",
+                imagem: "assets/prato3.jpg",
+                preco: "24,90"
+            }
+          ]
     }
-  ];
+];
 
-let array_pratos;
+let array_itens;
 let prod;
 
 for(let i in dados_cardapio) {
     
     prod = dados_cardapio[i];
-    // console.log(prod.categoria, prod.pratos);
-    // console.log("total de elementos: "+prod.pratos.length);
-
-    let num_id;
 
     let num = 1;
     
-    array_pratos = (prod.categoria, prod.pratos);
+    array_itens = (prod.categoria, prod.itens);
     
-    if((prod.categoria == 'ofertas_do_dia') && (prod.pratos.length > 0)){
-        
-        for(let e in prod.pratos){
+    if((prod.categoria == prod.categoria) && (prod.itens.length > 0)){
 
-            let conteudo = `<div class="box" id="oferta-destaque-${num}">
-                <div class="pratos">
-                    <img src="${prod.pratos[e].imagem}" alt="">
+        let title = `<h1 class="title_sessoes">${prod.title}</h1><div class="grid"  id="${prod.categoria}"></div>`;
+        document.querySelector('.conteudo').innerHTML += title;
+
+        let recebe_conteudo = prod.categoria;
+        console.log(`conteudo ${recebe_conteudo}`);
+        
+        for(let e in prod.itens){
+            let conteudo = `<div class="box" id="${prod.categoria}-${num}">
+                <div class="itens">
+                    <img src="${prod.itens[e].imagem}" alt="">
                 </div>
-                <div class="info-pratos">
-                    <h3 class="title">${prod.pratos[e].nome}</h3>
-                    <p class="descritivo">${prod.pratos[e].descricao}</p>
+                <div class="info-itens">
+                    <h3 class="title">${prod.itens[e].nome}</h3>
+                    <p class="descritivo">${prod.itens[e].descricao}</p>
                 </div>
                 <div class="seleciona-qtd seleciona_qtd_geral">
-                    <button onclick="escolherItens(this)" data-incremento="#oferta-destaque-${num}" name="btn-qtd-menos" class="btn-qtd btn-qtd-menos">-</button>
+                    <button onclick="escolherItens(this)" data-incremento="#${prod.categoria}-${num}" name="btn-qtd-menos" class="btn-qtd btn-qtd-menos">-</button>
                     <input type="text" name="quantidade" value="0" class="gtd-pedido">
-                    <button onclick="escolherItens(this)" data-incremento="#oferta-destaque-${num}"  name="btn-qtd-mais" class="btn-qtd btn-qtd-mais">+</button>
+                    <button onclick="escolherItens(this)" data-incremento="#${prod.categoria}-${num}"  name="btn-qtd-mais" class="btn-qtd btn-qtd-mais">+</button>
 
-                    <p class="price">R$ <span>${prod.pratos[e].preco}</span></p>
-                    <span class="price_auxiliar">${prod.pratos[e].preco}</span>
+                    <p class="price">R$ <span>${prod.itens[e].preco}</span></p>
+                    <span class="price_auxiliar">${prod.itens[e].preco}</span>
                 </div>
             </div>`;
-            document.querySelector('#section-1').innerHTML += conteudo;
+            document.querySelector(`#${recebe_conteudo}`).innerHTML += conteudo;
 
-            num++;
-        }
-        
-        // console.log("va;or de num id " + num_id);
-    }
-    if((prod.categoria == 'semanais') && (prod.pratos.length > 0)){
-
-        let title_diarios = `<h1>Pratos diários</h1><div class="destaques"  id="pratos_diarios"></div>`;
-        document.querySelector('#semanais').innerHTML += title_diarios;
-
-        for(let x in prod.pratos){
-            let conteudo = `
-                <div class="box" id="ofertas-diaria-${num}">
-                    <div class="pratos">
-                        <img src="${prod.pratos[x].imagem}" alt="">
-                    </div>
-                    <div class="info-pratos">
-                        <h3 class="title">${prod.pratos[x].nome}</h3>
-                        <p class="descritivo">${prod.pratos[x].descricao}</p>
-                    </div>
-                    <div class="seleciona-qtd seleciona_qtd_geral">
-                        <button onclick="escolherItens(this)" data-incremento="#ofertas-diaria-${num}" name="btn-qtd-menos" class="btn-qtd btn-qtd-menos">-</button>
-                        <input type="text" name="quantidade" value="0" class="gtd-pedido">
-                        <button onclick="escolherItens(this)" data-incremento="#ofertas-diaria-${num}" name="btn-qtd-mais" class="btn-qtd btn-qtd-mais">+</button>
-
-                        <p class="price">R$ <span>${prod.pratos[x].preco}</span></p>
-                        <span class="price_auxiliar">${prod.pratos[x].preco}</span>
-                    </div>
-                </div>`;
-            document.querySelector('#pratos_diarios').innerHTML += conteudo;
-            num++;
-        }
-    }
-    if((prod.categoria == 'sobremesas') && (prod.pratos.length > 0)){
-
-        let title_sobremesas = `<h1>Sobremesas</h1><div class="grid sobremesas"  id="sobremesas"></div>`;
-        document.querySelector('.sobremesas').innerHTML += title_sobremesas;
-
-        for(let y in prod.pratos){
-            let conteudo = `
-                <div class="box" id="sobremesas-${num}">
-                    <div class="pratos">
-                        <img src="${prod.pratos[y].imagem}" alt="">
-                    </div>
-                    <div class="info-pratos">
-                        <h3 class="title">${prod.pratos[y].nome}</h3>
-                        <p class="descritivo">${prod.pratos[y].descricao}</p>
-                    </div>
-                    <div class="seleciona-qtd seleciona_qtd_geral">
-                        <button onclick="escolherItens(this)" data-incremento="#sobremesas-${num}" name="btn-qtd-menos" class="btn-qtd btn-qtd-menos">-</button>
-                        <input type="text" name="quantidade" value="0" class="gtd-pedido">
-                        <button onclick="escolherItens(this)" data-incremento="#sobremesas-${num}" name="btn-qtd-mais" class="btn-qtd btn-qtd-mais">+</button>
-
-                        <p class="price">R$ <span>${prod.pratos[y].preco}</span></p>
-                        <span class="price_auxiliar">${prod.pratos[y].preco}</span>
-                    </div>
-                </div>`;
-            document.querySelector('#sobremesas').innerHTML += conteudo;
             num++;
         }
     }
 }
-
-
-// export default dados_cardapio;
--- js/script.js
+++ js/script.js
@@ -5,31 +5,33 @@ const c = (el) => document.querySelector(el);
 let headerHeight = c(".header").clientHeight;
 console.log(`${headerHeight}px`);
 
-c(".conteudo").style.marginTop = `${headerHeight}px`;
-c('.fazer-pedido').style.top = `${headerHeight - 4}px`;
+c(".conteudo").style.marginTop = `${headerHeight +20}px`;
+// c('.fazer-pedido').style.top = `${headerHeight - 4}px`;
 
 let incluirIdsUnicos = [];
 const salvarId = [];
-const pedidos= [];
+const pedidos = [];
 let cont = 1;
 
-let quantidadeItens;
-let price_atualizado;
+let quantidadeItens = 0;
+let price_atualizado = 0;
 let cta_name;
 
-function escolherItens(elemento){
+const qtd_carrinho = [];
+
+function escolherItens(elemento) {
     // captando o valor de texto do atributo data
     let value_data_incremento = elemento.getAttribute('data-incremento');
-    console.log('value data :'+value_data_incremento);
+    console.log('value data :' + value_data_incremento);
 
     cta_name = elemento.getAttribute('name');
 
     definirQuantidade(value_data_incremento, cta_name);
 }
 
-function definirQuantidade(value_data_incremento, cta_name){
+function definirQuantidade(value_data_incremento, cta_name) {
     //adiciona cada ID do box clicado ao array
-    salvarId.push({idDiv: value_data_incremento});
+    salvarId.push({ idDiv: value_data_incremento });
 
     // percorremos todo array e removemos IDs duplicados
     incluirIdsUnicos = new Set(salvarId.map(obj => obj.idDiv));
@@ -38,57 +40,80 @@ function definirQuantidade(value_data_incremento, cta_name){
 
     quantidadeItens = parseInt((c(`${value_data_incremento} .gtd-pedido`)).value);
 
-    price_atualizado; 
+    price_atualizado;
 
-    if (typeof(quantidadeItens) == "number"){
-        if(cta_name === "btn-qtd-mais"){
-            if(quantidadeItens >= 0){
+    if (typeof (quantidadeItens) == "number") {
+        if (cta_name === "btn-qtd-mais") {
+            if (quantidadeItens >= 0) {
                 c(`${value_data_incremento} .gtd-pedido`).setAttribute('value', (quantidadeItens + 1));
 
                 price_atualizado = (formatarMoeda(price, quantidadeItens + 1)).replace('R$ ', '');
                 c(`${value_data_incremento} .price span`).innerHTML = price_atualizado;
+                c(`${value_data_incremento}`).style.backgroundColor = '#a6888842';
+                quantidadeItens + 1;
+
+                // qtd_carrinho.push({ value_data_incremento: quantidadeItens });
+                console.log("quantidade de itens: +++ " + quantidadeItens + 1);
+
+                atualizarTotalItensCarrinho(quantidadeItens + 1);
+            }
+            else {
+                c(`${value_data_incremento}`).style.backgroundColor = '#fff';
+                console.log(quantidadeItens);
             }
         }
-        if(cta_name === "btn-qtd-menos"){
-            if(quantidadeItens >= 1){
+        if (cta_name === "btn-qtd-menos") {
+            if (quantidadeItens >= 1) {
                 c(`${value_data_incremento} .gtd-pedido`).setAttribute('value', (quantidadeItens - 1));
 
                 price_atualizado = (formatarMoeda(price, quantidadeItens - 1)).replace('R$ ', '');
                 c(`${value_data_incremento} .price span`).innerHTML = price_atualizado;
 
+                // qtd_carrinho.push({ value_data_incremento: quantidadeItens });
+                console.log("quantidade de itens --- : " + quantidadeItens - 1);
+
+                atualizarTotalItensCarrinho(quantidadeItens - 1);
             }
             else {
                 c(`${value_data_incremento} .gtd-pedido`).setAttribute('value', 0);
+                c(`${value_data_incremento}`).style.backgroundColor = '#fff';
 
                 c(`${value_data_incremento} .price span`).innerHTML = price;
+
+                console.log(quantidadeItens);
             }
         }
-    }else{
+    } else {
         throw "Tipo inválido";
-    }  
-}  
+    }
+}
+
+function atualizarTotalItensCarrinho(qtd) {
+    c('.qtd-itens-carrinho').innerHTML = qtd;
+
+}
 
 function abrirModal() {
 
-        if(incluirIdsUnicos.size > 0) {
+    if (incluirIdsUnicos.size > 0) {
 
-            incluirIdsUnicos.forEach(function(value) {
+        incluirIdsUnicos.forEach(function (value) {
 
-                let img = c(`${value} .pratos`).innerHTML;
-                let title = c(`${value} .title`).innerHTML;
-                let descritivo = c(`${value} .descritivo`).innerHTML;
-                let quantidade = c(`${value} .gtd-pedido`).value;
-                let price = c(`${value} .price span`).innerHTML;
-        
-                // const c = formatarMoeda(price, quantidade);
-                // const price_real = c.replace('R$ ', '');
-                console.log(price);
-        
-                if(quantidade != 0){
-        
-                    let id = value.replace('#', '');
-        
-                    let pedidos = `
+            let img = c(`${value} .itens`).innerHTML;
+            let title = c(`${value} .title`).innerHTML;
+            let descritivo = c(`${value} .descritivo`).innerHTML;
+            let quantidade = c(`${value} .gtd-pedido`).value;
+            let price = c(`${value} .price span`).innerHTML;
+
+            // const c = formatarMoeda(price, quantidade);
+            // const price_real = c.replace('R$ ', '');
+            console.log(price);
+
+            if (quantidade != 0) {
+
+                let id = value.replace('#', '');
+
+                let pedidos = `
                         <div class="box-item" id="${id}-modal">
                             ${img}
                             <div class="descricao-item">
@@ -102,30 +127,30 @@ function abrirModal() {
                             </div>
                         </div>
                     </div>`;
-        
-                    c('#pedido-itens').innerHTML += pedidos;
-                };
-                
-            });
 
-            //Abrir MODAL
-            c("#fazer-pedido").style.display = 'block';
+                c('#pedido-itens').innerHTML += pedidos;
+            };
 
-        }else{
-            alert('Nenhum pedido selecionado');
-        }
- }
+        });
 
- function fecharModal(){
+        //Abrir MODAL
+        c("#fazer-pedido").style.display = 'block';
+
+    } else {
+        alert('Nenhum pedido selecionado');
+    }
+}
+
+function fecharModal() {
     c("#fazer-pedido").style.display = 'none';
 
     let pedidos = ``;
     c('#pedido-itens').innerHTML = pedidos;
- }
+}
 
 function formatarMoeda(price, quantidade) {
     const a = price.replace(',', '.');
-    
+
     // Formatando para o formato de moeda brasileira:
     const formatter = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
@@ -137,7 +162,7 @@ function formatarMoeda(price, quantidade) {
     return price_final;
 }
 
-function escolherItensModal(elemento){
+function escolherItensModal(elemento) {
 
     // captando o valor de texto do atributo data
     let data_incremento = elemento.getAttribute('data-incremento'); // Pegando id do box modal
@@ -145,15 +170,15 @@ function escolherItensModal(elemento){
 
     let quantidadeModal = parseInt((c(`${data_incremento} .gtd-pedido`)).value); // pegando a quantidade de itens e convert INT
 
-    
+
     const price = c(`${aux} .price_auxiliar`).innerHTML; // pegando o preço do produto
-    
+
     cta_name = elemento.getAttribute('name');
 
     price_atualizado;
 
-    if(cta_name === "btn-qtd-mais"){
-        if(quantidadeModal > 0){
+    if (cta_name === "btn-qtd-mais") {
+        if (quantidadeModal > 0) {
             c(`${data_incremento} .gtd-pedido`).setAttribute('value', (quantidadeModal + 1));
             c(`${aux} .gtd-pedido`).setAttribute('value', (quantidadeModal + 1));
 
@@ -161,15 +186,15 @@ function escolherItensModal(elemento){
             c(`${data_incremento} .price span`).innerHTML = price_atualizado;
         }
     }
-    if(cta_name === "btn-qtd-menos"){
-        if(quantidadeModal >= 1){
-            console.log('quantidade no if apos clique: '+quantidadeModal)
+    if (cta_name === "btn-qtd-menos") {
+        if (quantidadeModal >= 1) {
+            console.log('quantidade no if apos clique: ' + quantidadeModal)
             c(`${data_incremento} .gtd-pedido`).setAttribute('value', (quantidadeModal - 1));
             c(`${aux} .gtd-pedido`).setAttribute('value', (quantidadeModal - 1));
 
             price_atualizado = (formatarMoeda(price, quantidadeModal - 1)).replace('R$ ', '');
             c(`${data_incremento} .price span`).innerHTML = price_atualizado;
-        }else{
+        } else {
             c(`${data_incremento}`).style.display = 'none';
             c(`${data_incremento} .gtd-pedido`).setAttribute('value', 0);
         }
@@ -177,18 +202,19 @@ function escolherItensModal(elemento){
 
 }
 
-// function mostrarDetalhes(elemento) {
-//     let a = elemento.offsetParent;
-//     let id_card= a.id;
+function logoMobile(){
+    document.querySelector(".menu").classList.toggle("_open");
+    document.querySelector(".menu-mobile").classList.toggle("_open");
 
-//     c(`#${id_card} .descritivo`).classList.toggle("_open");
-//     console.log(id_card);
-// }
-  
-            
+    document.querySelector("#line1").classList.toggle("_open");
+    document.querySelector("#line2").classList.toggle("_open");
+    document.querySelector("#line3").classList.toggle("_open");
+
+    document.querySelector(".fade").classList.toggle("_open");
+}
 
 
 
- 
+
 
 
